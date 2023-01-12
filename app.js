524b02214b65e30ef19d084dc7acd931eadda31e
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2023-01-12 19:12:48 +0300

    Unblocked Cors
    Unblocked Cors

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
-
 const app = express();
-
+app.use(cors());
 var corsOptions = {
   origin: "http://localhost:8081" // Cors ho an'ny côté front
 };
@@ -77,7 +76,20 @@ function initial() { // Initialisation role any am base de donnee
       }
     });
   }
+  var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
 //route de base
 app.get("/", (req, res) => {
