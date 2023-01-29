f401af2d12e68110840c13d962d29fe6529fabf3
sarobidy <ratsaraefadahy@gmail.com>
2023-01-29 23:52:11 +0300

    fonction historiques reparation

--- routes/reparation.routes.js
+++ routes/reparation.routes.js
@@ -18,5 +18,5 @@ module.exports = function(app) {
     app.post("/api/reparation/updateVehiculeTerminee/:vehicule", controller.updateVehiculeTerminee);
     app.get("/api/reparation/getFactureReparationParVoiture/:utilisateur/:vehicule", controller.getFactureReparationParVoiture);
     app.get("/api/reparation/getBondeSortieParVoiture/:utilisateur/:vehicule", controller.getBondeSortieParVoiture);
-    
+    app.get("/api/reparation/getHistoriqueReparation/:utilisateur/:vehicule", controller.getHistoriqueReparation);   
 }
