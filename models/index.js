6bfc2365a0a714a8b5d52d0d11307042602b5bb7
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2023-02-01 10:22:43 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 const db = {};
 
 db.mongoose = mongoose;
-mongoose.connect(process.env.MONGO_URI, {
+db.mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     strictQuery: true
