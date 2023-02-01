d26e251398d31997ff2df9c248a04f4cb7e893a4
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2023-02-01 10:20:57 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,7 +5,11 @@ mongoose.Promise = global.Promise;
 const db = {};
 
 db.mongoose = mongoose;
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    strictQuery: true
+  });
 db.utilisateur = require("./utilisateur.model");
 db.role = require("./role.model");
 db.reparation = require("./reparation.model");
